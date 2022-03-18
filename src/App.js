cb398837453e4db3b362c475a4aa548a2937ce02
Lepias <topiasleppakoski@gmail.com>
2022-03-18 23:58:27 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost/ostoslista/'
 
 function App() {
+  //input
+  const [items, setItems] = useState({
+    description:'',
+    amount:''
+  });
+  //output
+  const [item, setItem] = useState([]);
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setItems({
+      ...items,
+      [e.target.name]: value
+    });
+  }
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setItem(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }, []);
+
+  function add(e) {
+    e.preventDefault();
+
+    const json = JSON.stringify(items)
+    
+    axios.post(URL + 'add.php',json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      setItem(item => [...item, response.data]);
+      setItems({
+        description:'',
+        amount:''
+      });
+
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <form onSubmit={add}>
+        <label>New item:</label>
+        <input value={items.description} name='description' placeholder='type description' onChange={handleChange}/* { e=> setItems(e.target.value)} */ /> Amount:
+        <input value={items.amount} name='amount' placeholder='type amount' onChange={handleChange} />
+
+        <button>Add</button>
+      </form>
+
+      <ol>
+        {item?.map(item => (
+          <li key={item.id}>{item.description}{item.amount}</li>
+        ))}
+      </ol>
     </div>
+
   );
 }
 
